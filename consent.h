fbe6fa28efe5eddafa1f6372fec897e30fd9d31d
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-11-21 12:11:48 +0100

    Update consent.h

--- consent.h
+++ consent.h
@@ -1,6 +1,15 @@
+#include <avr/io.h>
+#include <inttypes.h>
+#include <util/delay.h>
+
 /*
   Fait clignoter la LED 10 secondes.
   Si bouton appuyé, on arrete le clignotement et on renvoie 1.
   Sinon on renvoie zero.
 */
 uint8_t askConsent();
+
+void delay_ms(uint16_t ms);
+void init_io();
+int button_is_pressed();
+void toggle_led();
