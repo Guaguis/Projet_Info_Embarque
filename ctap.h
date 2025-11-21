2c4e95f514443ecb7c7a95be3b286031785d085f
AkasHOSS42 <akashoss42@gmail.com>
2025-11-21 09:45:25 +0000

    Create ctap.h

---
+++ ctap.h
@@ -0,0 +1,7 @@
+char req[41];
+
+// repond a la requete
+void respond(void);
+
+// lit la requete -> declenche par interruption a priori, ou bien est bloquant jusqua reception requete
+void listen(void);
