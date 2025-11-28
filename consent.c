025a1cf15f3add0856f46d288e53b7f9f8e07e57
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-11-28 16:00:28 +0100

    Update consent.c

--- consent.c
+++ consent.c
@@ -2,6 +2,8 @@
 #include<avr/wdt.h>
 #include<avr/intterupt.h>
 #include <avr/sleep.h>
+#include <stdint.h>
+#include <avr/io.h>
 
 void setup_watchdog_for_16ms(){
    cli(); // disable global interrupts 
@@ -14,6 +16,30 @@ void setup_watchdog_for_16ms(){
  }
 
 ISR(WDT_vect){}
+DDRD &= ~_BV(PD2);  // PD2 en entrée
+PORTD |= _BV(PD2);
+
+volatile uint8_t button_state = 1;     // bouton sur pull-up
+volatile uint8_t counter = 0;
+volatile uint8_t button_pushed_flag = 0;
+
+void debounce() {
+    uint8_t current = (PIND & _BV(PD2)) ? 1 : 0;
+
+    if (current != button_state) {
+        counter++;
+        if (counter >= 4) {
+            button_state = current;
+            counter = 0;
+
+            if (button_state == 0) {    // appui détecté (active LOW)
+                button_pushed_flag = 1;
+            }
+        }
+    } else {
+        counter = 0;
+    }
+}
 
 
 uint8_t() wait_for_consent(){
