f57c27ccaf73fc0e7743e5ec922d8d5ffcf22111
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-12-04 19:31:07 +0100

    Update ctap.h

--- ctap.h
+++ ctap.h
@@ -1,13 +1,17 @@
-
-char req[41];
-
-// repond a la requete
-void respond(void);
-
-// lit la requete -> declenche par interruption a priori, ou bien est bloquant jusqua reception requete
-void listen(void);
+#ifndef CTAP_H
+#define CTAP_H
+#include <stdint.h>
+/*
+ * Implémentation des 4 commandes pour la communication suivant  CTAP  :
+ * - MakeCredential
+ * - GetAssertion
+ * - ListCredentials
+ * - Reset
+ * Ces fonctions doivent être appelées après lecture du byte de commande dans le main
+ */
 
 void makecredential(void);
 void getassertion(void); 
 void listcredentials(void);
 void reset(void); 
+#endif // CTAP_H
