877b804b8f5cca3314fd681493b420dd1df3903a
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-12-05 16:46:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,63 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include <stdint.h>
 
+#include "uart.h"
 #include "ctap.h"
 #include "memoire.h"
-#include "alea.h"
 #include "signature.h"
-#include "consent.h"
-#include <uart.h>
 
-#define COMMAND_LIST_CREDENTIALS (0)
-#define COMMAND_MAKE_CREDENTIAL (1)
-#define COMMAND_GET_ASSERTION (2)
-#define COMMAND_RESET (3)
+#define COMMAND_LIST_CREDENTIALS   0
+#define COMMAND_MAKE_CREDENTIAL    1
+#define COMMAND_GET_ASSERTION      2
+#define COMMAND_RESET              3
 
-#define STATUS_ERR_COMMAND_UNKNOWN (1)
+#define STATUS_ERR_COMMAND_UNKNOWN 1
 
-int main(void ){
+int main(void) {
     UART__init();
     memoire_init();
     signature_init();
-    consent_init();
-    
-for (;;) {
-    uint8_t cmd = UART__getc();
-    switch (cmd) {
-        case COMMAND_MAKE_CREDENTIAL: makecredential(); break;
-        case COMMAND_GET_ASSERTION:   getassertion();   break;
-        case COMMAND_LIST_CREDENTIALS: listcredentials(); break;
-        case COMMAND_RESET: reset(); break;
-        default: UART__putc(STATUS_ERR_COMMAND_UNKNOWN); break;
+    TCCR1A = 0;
+    TCCR1B |= _BV(CS11) | _BV(CS10);   // prescaler 64
+    sei();
+
+    //  (CPU idle)
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+
+    for (;;) {
+        
+        sleep_cpu();
+
+        uint8_t cmd;
+        if (!uart_read_from_buffer(&cmd)) {
+            switch (cmd) {
+                case COMMAND_MAKE_CREDENTIAL:
+                    makecredential();
+                    break;
+
+                case COMMAND_GET_ASSERTION:
+                    getassertion();
+                    break;
+
+                case COMMAND_LIST_CREDENTIALS:
+                    listcredentials();
+                    break;
+
+                case COMMAND_RESET:
+                    reset();
+                    break;
+
+                default:
+                    UART__putc(STATUS_ERR_COMMAND_UNKNOWN);
+                    break;
+            }
+        }
+        .
     }
+
+    return 0;
 }
-return 0 ;
-}
+
