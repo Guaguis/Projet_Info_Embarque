ebb2ec69f797a35a0f763972870fcf26c0af1745
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-12-05 16:45:13 +0100

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,28 +1,54 @@
-#include <uart.h>
+#include "uart.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
 #define FOSC (16000000UL) // Clock Speed
-#define BAUD (115200UL)
+#define BAUD (9600UL)
 #define MYUBRR ((FOSC/(16*BAUD))-1)
 
+static uint8_t buf[BUFSIZE];
+
+struct ring_buffer uart__rb;
+
 void UART__init(void){
-  UCSR0B=_BV(RXEN0)|_BV(TXEN0); // enable TX and RX
+  cli(); // recommande dans la doc de desactiver les interruptions quand on init
+  UCSR0B=_BV(RXEN0)|_BV(TXEN0)|_BV(RXCIE0); // enable interrupts, TX and RX
   UCSR0C=_BV(UCSZ00)|_BV(UCSZ01); // 8-bits data
 
   /*Set baud rate */
   UBRR0H = (unsigned char)(MYUBRR>>8);
   UBRR0L = (unsigned char)MYUBRR;
+
+  ring_buffer__init(&uart__rb, buf, BUFSIZE);
+  sei();
 }
 
-uint8_t UART__getc(void){
-  /* Wait for data to be received */
-  while ( !(UCSR0A & _BV(RXC0)) ){}
-  /* Get and return received data from buffer */
-  return UDR0;
+ISR(USART_RX_vect){
+  cli();
+  ring_buffer__push(&uart__rb, UDR0);
+  sei();
 }
 
 void UART__putc(uint8_t data){
-  /* Wait for empty transmit buffer */
-  while ( !( UCSR0A & _BV(UDRE0)) ){}
-  /* Put data into buffer, sends the data */
+  while (!(UCSR0A & _BV(UDRE0))) {
+    /* attente courte*/
+  }
   UDR0 = data;
 }
+uint8_t uart_data_ready(void){
+  /* s'il y a au moins 1 octet dans le ring buffer */
+  return (uart__rb.head != uart__rb.tail);
+}
+uint8_t uart_read_from_buffer(uint8_t *data){
+  /* ring_buffer__pop retourne 0 si OK, 1 si vide */
+  return ring_buffer__pop(&uart__rb, data);
+}
+uint8_t usart_read_byte(void){
+  uint8_t data;
+  /* on boucle tant que le buffer est vide */
+  while (ring_buffer__pop(&uart__rb, &data)) {
+    
+  }
+  return data;
+}
+
