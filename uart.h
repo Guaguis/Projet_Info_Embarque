c0875b7476f03d8c4bdf02726eb422a06fc77486
Guaguis <158030769+Guaguis@users.noreply.github.com>
2025-12-05 16:46:02 +0100

    Update uart.h

--- uart.h
+++ uart.h
@@ -2,9 +2,24 @@
 #define UART_H
 
 #include <stdint.h>
+#include "ring_buffer.h"
+
+#define BUFSIZE 8
 
 void UART__init(void);
-uint8_t UART__getc(void);
-void UART__putc(uint8_t);
+
+/* Envoie 1 octet sur l’UART */
+void UART__putc(uint8_t data);
+
+/* Indique s’il y a au moins 1 octet dans le buffer RX */
+uint8_t uart_data_ready(void);
+
+/* Lit 1 octet du buffer RX (non bloquant)
+ *  retourne 0 si OK, 1 si buffer vide
+ */
+uint8_t uart_read_from_buffer(uint8_t *data);
+
+/* Version bloquante : attend jusqu'à avoir un octet */
+uint8_t usart_read_byte(void);
 
 #endif // UART_H
